21a84b039187eb96f52d127c0a03d1b399c9a7f9
petoetje <petoetje@github>
2014-06-26 14:30:12 +0200

    should not make difference

--- xorg/X11R7.6/rdp/rdpinput.c
+++ xorg/X11R7.6/rdp/rdpinput.c
@@ -331,6 +331,7 @@ rdpLoadLayout(int keylayout)
             break;
         case 0x00000813: /* Belgian */
             set.layout = "be";
+	    set.rules = "evdev";
             break;
         case 0x00000816: /* Portuguese */
             set.layout = "pt";
